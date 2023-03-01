8852d1511a46ce016f513791b3a8f02d18d3ba40
Javier <ingenierodesoftware73@gmail.com>
2023-03-01 10:15:06 -0500

    edit view created

---
+++ application/view/computers/edit.php
@@ -0,0 +1,18 @@
+<div class="container">
+    <h2>You are in the View: application/view/computers/edit.php (everything in this box comes from that file)</h2>
+    <!-- add book form -->
+    <div>
+        <h3>Edit a computer</h3>
+        <form action="<?php echo URL; ?>computer/updatecomputer" method="POST">
+            <label>Brand</label>
+            <input autofocus type="text" name="brand" value="<?php echo htmlspecialchars($computer->brand, ENT_QUOTES, 'UTF-8'); ?>" required />
+            <label>Description</label>
+            <input type="text" name="description" value="<?php echo htmlspecialchars($computer->description, ENT_QUOTES, 'UTF-8'); ?>" required />
+            <label>Link</label>
+            <input type="text" name="link" value="<?php echo htmlspecialchars($computer->link, ENT_QUOTES, 'UTF-8'); ?>" />
+            <input type="hidden" name="computer_id" value="<?php echo htmlspecialchars($computer->id, ENT_QUOTES, 'UTF-8'); ?>" />
+            <input type="submit" name="submit_update_computer" value="Update" />
+        </form>
+    </div>
+</div>
+
