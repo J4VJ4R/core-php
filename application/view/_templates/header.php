223099641d7371fac70d839f3274f265502f83b4
Javier <ingenierodesoftware73@gmail.com>
2023-03-13 04:51:00 -0500

    some changes

--- application/view/_templates/header.php
+++ application/view/_templates/header.php
@@ -12,6 +12,9 @@
 
     <!-- CSS -->
     <link href="<?php echo URL; ?>css/style.css" rel="stylesheet">
+    <!-- Bootstrap CSS v5.2.1 -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+		integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 </head>
 <body>
     <!-- logo, check the CSS file for more info how the logo "image" is shown -->
